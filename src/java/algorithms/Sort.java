b0387e7d0087266a7b37d931bb2a0114ca071114
Danny Chen <dannychen8102@gmail.com>
2022-05-02 15:05:48 -0400

    Initial commit

---
+++ src/java/algorithms/Sort.java
@@ -0,0 +1,23 @@
+package src.java.algorithms;
+public class Sort {
+    private int[] arr;
+
+    public Sort(int[] arr) {
+        this.arr = arr;
+    }
+    
+    public void beginSelectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {  
+            int minNum = i;  
+            for (int j = i + 1; j < arr.length; j++) {  
+                if (arr[j] < arr[minNum]) {  
+                    minNum = j;  
+                }  
+            }  
+            int temp = arr[minNum]; // Swapping.
+            arr[minNum] = arr[i];  
+            arr[i] = temp;  
+        }  
+    }
+}
+
---
+++ src/java/gui/GraphGUI.java
@@ -0,0 +1,45 @@
+package src.java.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class GraphGUI {
+    private JFrame frame;
+    private JPanel graphPanel;
+    private JPanel graph;
+    private static final int windowWidth = 1280;
+    private static final int windowHeight = 720;
+
+     public GraphGUI() {
+        /* Initialization of the 'Graph JFrame' */
+        frame = new JFrame();
+        frame.setTitle("Algorithm Visualizer");
+        frame.setSize(windowWidth, windowHeight);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Fully closes frame when exited.
+
+        /* Graph Outer Panel */
+        graphPanel = new JPanel();
+        graphPanel.setBackground(Color.red);
+        graphPanel.setBounds(0, 0, 250, 250);
+
+        class displayArray {
+
+
+            public displayArray() {
+                
+            }
+
+            public void draw(Graphics graphics) {
+                
+            }
+        }
+
+        
+        /* Display frame */
+        frame.add(graphPanel);
+        frame.setVisible(true);
+    }
+}
