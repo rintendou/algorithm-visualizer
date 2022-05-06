a3dccb184309db446af29679ee77cde85a68fcc7
Danny Chen <dannychen8102@gmail.com>
2022-05-06 15:57:14 -0400

    Updated sorting amethods to sync with graphic animations

--- src/java/Sort.java
+++ src/java/Sort.java
@@ -1,23 +1,54 @@
-package src.java.algorithms;
-public class Sort {
-    private int[] arr;
+package src.java;
 
-    public Sort(int[] arr) {
-        this.arr = arr;
+import javax.swing.JPanel;
+
+import src.java.gui.SortingGUI;
+
+public class Sort implements Runnable {
+    private int[] sortingArray;
+    private SortingGUI f;
+    private String algo;
+
+    public Sort(int[] sortingArray, SortingGUI f, String algo) {
+        this.sortingArray = sortingArray;
+        this.f = f;
+        this.algo = algo;
     }
     
-    public void beginSelectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {  
-            int minNum = i;  
-            for (int j = i + 1; j < arr.length; j++) {  
-                if (arr[j] < arr[minNum]) {  
+    public void beginSelectionSort(int[] sortingArray) {
+        int temp;
+        int minNum;
+        for (int i = 0; i < sortingArray.length - 1; i++) {  
+            minNum = i;  
+            for (int j = i + 1; j < sortingArray.length; j++) {  
+                if (sortingArray[j] < sortingArray[minNum]) {  
                     minNum = j;  
                 }  
-            }  
-            int temp = arr[minNum]; // Swapping.
-            arr[minNum] = arr[i];  
-            arr[i] = temp;  
+            }
+            f.redraw(sortingArray); 
+
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException ex) {
+                ex.getSuppressed();
+             }
+
+            temp = sortingArray[minNum]; // Swapping.
+            sortingArray[minNum] = sortingArray[i];  
+            sortingArray[i] = temp;  
         }  
+    }
+
+    @Override
+    public void run() {
+        String selectedAgo = algo;
+        switch (selectedAgo) {
+            case "Selection Sort": 
+                beginSelectionSort(sortingArray);
+                break;
+            default: break;
+        }
+        f.sorting = false;
     }
 }
 
